bfee78222bf3d1d7034606a2bb2be5bb2115b5bf
TimGeDev <timgebauer.ldr@gmail.com>
2022-07-13 23:15:40 +0200

    * refactoring code with more querys

--- Logic/EPEverCommunicationHandler.cs
+++ Logic/EPEverCommunicationHandler.cs
@@ -8,25 +8,34 @@ namespace RaspTracer_AN_Modbus.Logic
         public const int TimeoutMs = 1000;
         public const string TTYPath = "/dev/ttyUSB0";
         public const int BaudRate = 115200;
+        private static SerialPort SerialPort;
 
+
+        public static void OpenConnection()
+        {
+            SerialPort = new SerialPort(TTYPath, BaudRate);
+            SerialPort.Open();
+        }
+        public static void CloseConnection()
+        {
+            SerialPort.Close();
+        }
 
         public static Byte[] GetValueFromEPever(Models.EPEverQueryPackage queryPackage)
         {
-            var port = new SerialPort(TTYPath, BaudRate);
-            port.Open();
 
             if (Program.debug)
             {
                 Console.WriteLine("Write: " + ByteArrayToString(queryPackage.GetQueryMessage()));
             }
-
-            port.Write(queryPackage.GetQueryMessage(), 0, 8);
+            
+            SerialPort.Write(queryPackage.GetQueryMessage(), 0, 8);
 
             long startMillis = DateTimeOffset.Now.ToUnixTimeMilliseconds();
-            while (port.BytesToRead == 0)
+            while (SerialPort.BytesToRead == 0)
             {
                 long currentMillis = DateTimeOffset.Now.ToUnixTimeMilliseconds();
-                if ((startMillis + TimeoutMs) > currentMillis)
+                if ((startMillis + TimeoutMs) < currentMillis)
                 {
                     Console.WriteLine("Timeout reached");
                     return Array.Empty<byte>();
@@ -34,9 +43,9 @@ namespace RaspTracer_AN_Modbus.Logic
                 Thread.Sleep(10);
             }
 
-            byte[] buffer = new byte[port.BytesToRead];
-            port.Read(buffer, 0, buffer.Length);
-            port.Close();
+            byte[] buffer = new byte[SerialPort.BytesToRead];
+            SerialPort.Read(buffer, 0, buffer.Length);
+            Console.WriteLine("length:" + SerialPort.BytesToRead);
 
             if (Program.debug)
             {
--- Program.cs
+++ Program.cs
@@ -1,25 +1,50 @@
 ﻿using System.IO.Ports;
+using Newtonsoft.Json;
 
 namespace RaspTracer_AN_Modbus;
 internal class Program
 {
     public const bool debug = true;
-
-    public static byte[] BATTERY_VOLTAGE = new byte[] { 0x04, 0x33, 0x1A, 0x00, 0x01 };
+    public static readonly Dictionary<string, byte[]> parametersToRetrieve = new Dictionary<string, byte[]>
+        {
+            { "LOAD_VOLTAGE", new byte[] { 0x04, 0x31, 0x0C, 0x00, 0x01 } },
+            { "LOAD_CURRENT", new byte[] { 0x04, 0x31, 0x0D, 0x00, 0x01 } },
+            { "SOLAR_VOLTAGE", new byte[] { 0x04, 0x31, 0x00, 0x00, 0x01 } },
+            { "SOLAR_CURRENT", new byte[] { 0x04, 0x31, 0x01, 0x00, 0x01 } },
+            { "BATTERY_VOLTAGE", new byte[] { 0x04, 0x33, 0x1A, 0x00, 0x01 } },
+           // { "BATTERY_AMPERAGE", new byte[] { 0x04, 0x33, 0x1A, 0x00, 0x01 } },
+            { "BATTERY_TEMPERATURE", new byte[] { 0x04, 0x33, 0x10, 0x00, 0x01 } },
+            { "DEVICE_TEMPERATURE", new byte[] { 0x04, 0x31, 0x11, 0x00, 0x01 } },
+        };
 
     private static async Task Main(string[] args)
     {
 
+
         Console.WriteLine("Starting Up....");
 
-        var queryPackage = new Models.EPEverQueryPackage(BATTERY_VOLTAGE);
+        Logic.EPEverCommunicationHandler.OpenConnection();
 
-        byte[] response = Logic.EPEverCommunicationHandler.GetValueFromEPever(queryPackage);
+        string values = RetrieveParameters();
+        Console.WriteLine("Reporting values: " + values);
 
-        Console.WriteLine("Result: " + queryPackage.GetValue(response));
+        await Logic.MQTTHandler.SendToHassAsync(values);
 
-        await Logic.MQTTHandler.SendToHassAsync(queryPackage.GetValue(response).ToString().Replace(",", "."));
+        Logic.EPEverCommunicationHandler.CloseConnection();
+    }
 
+    private static string RetrieveParameters()
+    {
+        var results = new Dictionary<string, string>();
+
+        foreach (var param in parametersToRetrieve)
+        {
+            var queryPackage = new Models.EPEverQueryPackage(param.Value);
+            byte[] response = Logic.EPEverCommunicationHandler.GetValueFromEPever(queryPackage);
+            string value = queryPackage.GetValue(response).ToString().Replace(",", ".");
+            results.Add(param.Key, value);
+        }
+        return JsonConvert.SerializeObject(results, Formatting.None);
 
     }
 
