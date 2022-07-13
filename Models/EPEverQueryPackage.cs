815024974eb154cf9ea2c6dbf30a07a29a099adb
TimGeDev <timgebauer.ldr@gmail.com>
2022-07-13 21:44:14 +0200

    * using correct amounts of source bytes for crc calculation

--- Models/EPEverQueryPackage.cs
+++ Models/EPEverQueryPackage.cs
@@ -57,7 +57,7 @@ namespace RaspTracer_AN_Modbus.Models
         {
             var param = new CRC.Parameters("CRC-16/MODBUS", 16, 0x8005, 0xFFFF, true, true, 0x0, 0x4B37);
             var crcClass = new CRC.Crc(param);
-            ulong crc = crcClass.ComputeCrc(0xFFFF, byteRequest, 0, 7);
+            ulong crc = crcClass.ComputeCrc(0xFFFF, byteRequest, 0, 6);
             var result = BitConverter.GetBytes(crc);
             this.SetBytePart(6, result[0]);
             this.SetBytePart(7, result[1]);
