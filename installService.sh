3af3dd22083cfdbdeaaca4163219ab3bab039cd3
timgebauer <Gebauer@kk-software.de>
2023-05-06 19:11:33 +0200

    Added Systemd Service

---
+++ installService.sh
@@ -0,0 +1,5 @@
+﻿#/bin/bash
+sudo cp -v RaspiTracer.service /etc/systemd/system #copy service file
+sudo systemctl enable RaspiTracer.service          #enable service
+sudo systemctl start RaspiTracer.service           #start service
+sudo systemctl status RaspiTracer.service          #check if the service started
